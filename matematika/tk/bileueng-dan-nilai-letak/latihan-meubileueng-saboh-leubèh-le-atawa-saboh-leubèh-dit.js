ef44f47a3c797866c79572ce5b98d55d07f134d7
atjeh-meurunoe <atjeh.meurunoe@gmail.com>
2025-04-22 21:05:06 +0700

    menerjemahkan lagi beberapa kata ya

---
+++ matematika/tk/bileueng-dan-nilai-letak/latihan-meubileueng-saboh-leubèh-le-atawa-saboh-leubèh-dit.js
@@ -0,0 +1,33 @@
+let kolomJawaban1 = document.getElementById("kolomjawaban1");
+let tombolJawaban1 = document.getElementById('cekjawaban1');
+let hasilJawaban1 = document.getElementById('jawaban1');
+
+//Periksa jawaban no. 1
+let j1 = true;
+
+kolomJawaban1.addEventListener('submit', function(event) {
+  event.preventDefault();
+  let opsi1_2 = document.getElementById('opsi2_2');
+
+  if (opsi2_2.checked == true) {
+    if (j2 == true) {
+      hasilJawaban2.innerHTML = 'Beutôi';
+      tombolJawaban2.innerHTML = 'Tôp Jawaban';
+      j2 = false;
+    } else {
+      hasilJawaban2.innerHTML = '';
+      tombolJawaban2.innerHTML = 'Peuréksa Jawaban';
+      j2 = true;
+    }
+  } else {
+    if (j2 == true) {
+      hasilJawaban2.innerHTML = 'Salah';
+      tombolJawaban2.innerHTML = 'Tôp Jawaban';
+      j2 = false;
+    } else {
+      hasilJawaban2.innerHTML = '';
+      tombolJawaban2.innerHTML = 'Peuréksa Jawaban';
+      j2 = true;
+    }
+  }
+})
