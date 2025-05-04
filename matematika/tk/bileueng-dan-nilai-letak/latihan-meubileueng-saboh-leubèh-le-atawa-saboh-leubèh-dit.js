16d8f020822c4a76959634529d16d427e0e6cbb5
atjeh-meurunoe <atjeh.meurunoe@gmail.com>
2025-05-04 10:33:15 +0700

    menerjemahkan lagi beberapa kata ya

--- matematika/tk/bileueng-dan-nilai-letak/latihan-meubileueng-saboh-leubèh-le-atawa-saboh-leubèh-dit.js
+++ matematika/tk/bileueng-dan-nilai-letak/latihan-meubileueng-saboh-leubèh-le-atawa-saboh-leubèh-dit.js
@@ -2,14 +2,60 @@ let kolomJawaban1 = document.getElementById("kolomjawaban1");
 let tombolJawaban1 = document.getElementById('cekjawaban1');
 let hasilJawaban1 = document.getElementById('jawaban1');
 
+let kolomJawaban2 = document.getElementById("kolomjawaban2");
+let tombolJawaban2 = document.getElementById('cekjawaban2');
+let hasilJawaban2 = document.getElementById('jawaban2');
+
+let kolomJawaban3 = document.getElementById("kolomjawaban3");
+let tombolJawaban3 = document.getElementById('cekjawaban3');
+let hasilJawaban3 = document.getElementById('jawaban3');
+
+let kolomJawaban4 = document.getElementById("kolomjawaban4");
+let tombolJawaban4 = document.getElementById('cekjawaban4');
+let hasilJawaban4 = document.getElementById('jawaban4');
+
+let kolomJawaban5 = document.getElementById("kolomjawaban5");
+let tombolJawaban5 = document.getElementById('cekjawaban5');
+let hasilJawaban5 = document.getElementById('jawaban5');
+
 //Periksa jawaban no. 1
 let j1 = true;
 
 kolomJawaban1.addEventListener('submit', function(event) {
   event.preventDefault();
-  let opsi1_2 = document.getElementById('opsi2_2');
+  let opsi1_2 = document.getElementById('opsi1_2');
 
-  if (opsi2_2.checked == true) {
+  if (opsi1_2.checked == true) {
+    if (j1 == true) {
+      hasilJawaban1.innerHTML = 'Beutôi';
+      tombolJawaban1.innerHTML = 'Tôp Jawaban';
+      j1 = false;
+    } else {
+      hasilJawaban1.innerHTML = '';
+      tombolJawaban1.innerHTML = 'Peuréksa Jawaban';
+      j1 = true;
+    }
+  } else {
+    if (j1 == true) {
+      hasilJawaban1.innerHTML = 'Salah';
+      tombolJawaban1.innerHTML = 'Tôp Jawaban';
+      j1 = false;
+    } else {
+      hasilJawaban1.innerHTML = '';
+      tombolJawaban1.innerHTML = 'Peuréksa Jawaban';
+      j1 = true;
+    }
+  }
+})
+
+//Periksa jawaban no. 2
+let j2 = true;
+
+kolomJawaban2.addEventListener('submit', function(event) {
+  event.preventDefault();
+  let opsi2_3 = document.getElementById('opsi2_3');
+
+  if (opsi2_3.checked == true) {
     if (j2 == true) {
       hasilJawaban2.innerHTML = 'Beutôi';
       tombolJawaban2.innerHTML = 'Tôp Jawaban';
@@ -28,6 +74,96 @@ kolomJawaban1.addEventListener('submit', function(event) {
       hasilJawaban2.innerHTML = '';
       tombolJawaban2.innerHTML = 'Peuréksa Jawaban';
       j2 = true;
+    }
+  }
+})
+
+//Periksa jawaban no. 3
+let j3 = true;
+
+kolomJawaban3.addEventListener('submit', function(event) {
+  event.preventDefault();
+  let opsi3_1 = document.getElementById('opsi3_1');
+
+  if (opsi3_1.checked == true) {
+    if (j3 == true) {
+      hasilJawaban3.innerHTML = 'Beutôi';
+      tombolJawaban3.innerHTML = 'Tôp Jawaban';
+      j3 = false;
+    } else {
+      hasilJawaban3.innerHTML = '';
+      tombolJawaban3.innerHTML = 'Peuréksa Jawaban';
+      j3 = true;
+    }
+  } else {
+    if (j3 == true) {
+      hasilJawaban3.innerHTML = 'Salah';
+      tombolJawaban3.innerHTML = 'Tôp Jawaban';
+      j3 = false;
+    } else {
+      hasilJawaban3.innerHTML = '';
+      tombolJawaban3.innerHTML = 'Peuréksa Jawaban';
+      j3 = true;
+    }
+  }
+})
+
+//Periksa jawaban no. 4
+let j4 = true;
+
+kolomJawaban4.addEventListener('submit', function(event) {
+  event.preventDefault();
+  let opsi4_3 = document.getElementById('opsi4_3');
+
+  if (opsi4_3.checked == true) {
+    if (j4 == true) {
+      hasilJawaban4.innerHTML = 'Beutôi';
+      tombolJawaban4.innerHTML = 'Tôp Jawaban';
+      j4 = false;
+    } else {
+      hasilJawaban4.innerHTML = '';
+      tombolJawaban4.innerHTML = 'Peuréksa Jawaban';
+      j4 = true;
+    }
+  } else {
+    if (j4 == true) {
+      hasilJawaban4.innerHTML = 'Salah';
+      tombolJawaban4.innerHTML = 'Tôp Jawaban';
+      j4 = false;
+    } else {
+      hasilJawaban4.innerHTML = '';
+      tombolJawaban4.innerHTML = 'Peuréksa Jawaban';
+      j4 = true;
+    }
+  }
+})
+
+//Periksa jawaban no. 5
+let j5 = true;
+
+kolomJawaban5.addEventListener('submit', function(event) {
+  event.preventDefault();
+  let opsi5_2 = document.getElementById('opsi5_2');
+
+  if (opsi5_2.checked == true) {
+    if (j5 == true) {
+      hasilJawaban5.innerHTML = 'Beutôi';
+      tombolJawaban5.innerHTML = 'Tôp Jawaban';
+      j5 = false;
+    } else {
+      hasilJawaban5.innerHTML = '';
+      tombolJawaban5.innerHTML = 'Peuréksa Jawaban';
+      j5 = true;
+    }
+  } else {
+    if (j5 == true) {
+      hasilJawaban5.innerHTML = 'Salah';
+      tombolJawaban5.innerHTML = 'Tôp Jawaban';
+      j5 = false;
+    } else {
+      hasilJawaban5.innerHTML = '';
+      tombolJawaban5.innerHTML = 'Peuréksa Jawaban';
+      j5 = true;
     }
   }
 })
