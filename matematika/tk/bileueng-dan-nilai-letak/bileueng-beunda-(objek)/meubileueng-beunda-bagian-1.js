b8a2cbbdd37b43f72ef4acdb782ea5511a7f6786
atjeh-meurunoe <atjeh.meurunoe@gmail.com>
2025-05-18 11:46:27 +0700

    menerjemahkan lagi beberapa kata ya

---
+++ matematika/tk/bileueng-dan-nilai-letak/bileueng-beunda-(objek)/meubileueng-beunda-bagian-1.js
@@ -0,0 +1,111 @@
+let kolomJawaban1 = document.getElementById("kolomjawaban1");
+let tombolJawaban1 = document.getElementById('cekjawaban1');
+let hasilJawaban1 = document.getElementById('jawaban1');
+
+let kolomJawaban2 = document.getElementById("kolomjawaban2");
+let tombolJawaban2 = document.getElementById('cekjawaban2');
+let hasilJawaban2 = document.getElementById('jawaban2');
+
+let kolomJawaban3 = document.getElementById("kolomjawaban3");
+let tombolJawaban3 = document.getElementById('cekjawaban3');
+let hasilJawaban3 = document.getElementById('jawaban3');
+
+let kolomJawaban4 = document.getElementById("kolomjawaban4");
+let tombolJawaban4 = document.getElementById('cekjawaban4');
+let hasilJawaban4 = document.getElementById('jawaban4');
+
+let kolomJawaban5 = document.getElementById("kolomjawaban5");
+let tombolJawaban5 = document.getElementById('cekjawaban5');
+let hasilJawaban5 = document.getElementById('jawaban5');
+
+//Periksa jawaban no. 1
+let j1 = true;
+
+kolomJawaban1.addEventListener('submit', function(event) {
+  event.preventDefault();
+  let opsi1_2 = document.getElementById('opsi1_2');
+
+  if (opsi1_2.checked == true) {
+    if (j1 == true) {
+      hasilJawaban1.innerHTML = 'Beutôi';
+      tombolJawaban1.innerHTML = 'Tôp Jawaban';
+      j1 = false;
+    } else {
+      hasilJawaban1.innerHTML = '';
+      tombolJawaban1.innerHTML = 'Peuréksa Jawaban';
+      j1 = true;
+    }
+  } else {
+    if (j1 == true) {
+      hasilJawaban1.innerHTML = 'Salah';
+      tombolJawaban1.innerHTML = 'Tôp Jawaban';
+      j1 = false;
+    } else {
+      hasilJawaban1.innerHTML = '';
+      tombolJawaban1.innerHTML = 'Peuréksa Jawaban';
+      j1 = true;
+    }
+  }
+})
+
+//Perisksa Jawaban No. 2
+let j2 = true;
+
+kolomJawaban2.addEventListener('submit', function(event) {
+  event.preventDefault();
+  let input2_1 = document.getElementById('input2_1');
+
+  if (input2_1.value == 18) {
+    if (j2 == true) {
+      hasilJawaban2.innerHTML = 'Beutôi';
+      tombolJawaban2.innerHTML = 'Tôp Jawaban';
+      j2 = false;
+    } else {
+      hasilJawaban2.innerHTML = '';
+      tombolJawaban2.innerHTML = 'Peuréksa Jawaban';
+      j2 = true;
+    }
+  } else {
+    if (j2 == true) {
+      hasilJawaban2.innerHTML = 'Salah';
+      tombolJawaban2.innerHTML = 'Tôp Jawaban';
+      j2 = false;
+    } else {
+      hasilJawaban2.innerHTML = '';
+      tombolJawaban2.innerHTML = 'Peuréksa Jawaban';
+      j2 = true;
+    }
+  }
+  
+})
+
+//Perisksa Jawaban No. 3
+let j3 = true;
+
+kolomJawaban3.addEventListener('submit', function(event) {
+  event.preventDefault();
+  let input3_1 = document.getElementById('input3_1');
+
+  if (input3_1.value == 6) {
+    if (j3 == true) {
+      hasilJawaban3.innerHTML = 'Beutôi';
+      tombolJawaban3.innerHTML = 'Tôp Jawaban';
+      j3 = false;
+    } else {
+      hasilJawaban3.innerHTML = '';
+      tombolJawaban3.innerHTML = 'Peuréksa Jawaban';
+      j3 = true;
+    }
+  } else {
+    if (j3 == true) {
+      hasilJawaban3.innerHTML = 'Salah';
+      tombolJawaban3.innerHTML = 'Tôp Jawaban';
+      j3 = false;
+    } else {
+      hasilJawaban3.innerHTML = '';
+      tombolJawaban3.innerHTML = 'Peuréksa Jawaban';
+      j3 = true;
+    }
+  }
+  
+})
