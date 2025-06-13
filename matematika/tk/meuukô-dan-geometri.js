6da06540d5456fae94ca0a043259cd26cf7f2fa0
atjeh-meurunoe <atjeh.meurunoe@gmail.com>
2025-06-13 14:03:10 +0700

    menerjemahkan lagi beberapa kata ya

---
+++ matematika/tk/meuukô-dan-geometri.js
@@ -0,0 +1,11 @@
+function quiz1() {
+    window.location.href = "meuukô-dan-geometri/quiz-1.html"
+}
+
+function quiz2() {
+    window.location.href = "meuukô-dan-geometri/quiz-2.html"
+}
+
+function ujianBab() {
+    window.location.href = "meuukô-dan-geometri/ujian-bab.html"
+}
