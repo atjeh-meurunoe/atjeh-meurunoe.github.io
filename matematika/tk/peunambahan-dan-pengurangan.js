8155a8af42eab542449b43da4acdb5b4e846d61b
atjeh-meurunoe <atjeh.meurunoe@gmail.com>
2025-06-13 11:18:49 +0700

    menerjemahkan lagi beberapa kata ya

---
+++ matematika/tk/peunambahan-dan-pengurangan.js
@@ -0,0 +1,11 @@
+function quiz1() {
+    window.location.href = "peunambahan-dan-pengurangan/quiz-1.html"
+}
+
+function quiz2() {
+    window.location.href = "peunambahan-dan-pengurangan/quiz-2.html"
+}
+
+function ujianBab() {
+    window.location.href = "peunambahan-dan-pengurangan/ujian-bab.html"
+}
---
+++ matematika/tk/tamah-ngön-kureueng.js
@@ -0,0 +1,11 @@
+function quiz1() {
+    window.location.href = "bileueng-dan-nilai-letak/quiz-1.html"
+}
+
+function quiz2() {
+    window.location.href = "bileueng-dan-nilai-letak/quiz-2.html"
+}
+
+function ujianBab() {
+    window.location.href = "bileueng-dan-nilai-letak/ujian-bab.html"
+}
